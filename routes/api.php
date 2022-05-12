310a144f35c76245622acfa0b30b9d608d8d34bf
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-05-12 16:31:07 +0700

    feat: add api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    $server = config('app.name');
+
+    $users = User::limit(1000)
+        ->inRandomOrder()
+        ->get()
+        ->shuffle()
+        ->transform(function ($item) use ($server) {
+            $item->server_name = $server;
+
+            return $item;
+        })
+        ->shuffle()
+        ->shuffle()
+        ->take(100);
+
+    return response()->json($users);
 });
